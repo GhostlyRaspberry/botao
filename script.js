e88a46308269fd1a7d831d0c8bb5be496196e3e9
GhostlyRaspberry <54464176+GhostlyRaspberry@users.noreply.github.com>
2020-03-17 02:45:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//Função para abrir links
+function abrirLink(url) {
+  window.open(url);
+}
+
+
+
 //Função do evento "clck" principal
 var contador = 0;
 
@@ -26,10 +33,37 @@ function botaoQuaseInutil() {
     document.getElementById("lenda").innerHTML = 'Lendas dizem que se o botão for apertado mais ' + ((contador - 5) *-1) + ' vezes, algo acontecerá!';
   }
   if (contador == 5) {
-    document.getElementById("textoTooltip").innerHTML = 'Este é o fim!'
+    document.getElementById("textoTooltip").innerHTML = 'Será que este é o fim?'
     document.getElementById("lenda").innerHTML = 'Viu? Algo aconteceu! Você desbloqueou o Dark Mode!';
     document.getElementById('body').style.backgroundColor = '#1a1a1a';
     document.getElementById('body').style.color = '#fff';
+    document.getElementById('myModal').style.color = '#000';
+  }
+  if (contador == 6) {
+    document.getElementById("textoTooltip").innerHTML = 'Continue clickando...'
+  }
+  if (contador == 8) {
+    document.getElementById("lenda").innerHTML = 'Você parece estar se divertindo apertando este botão praticamente inútil.';
+  }
+  if (contador == 9) {
+    document.getElementById("lenda").innerHTML = 'Não posso te culpar, nesses tempos de corona virus, é difícil achar o que fazer.';
+  }
+  if (contador == 10) {
+    document.getElementById("lenda").innerHTML = 'Vou tentar te ajudar, aperte mais uma vez pra ter uma maneira ainda melhor de disperdiçar seu tempo.';
+  }
+  if (contador == 11) {
+    abrirLink("https://theuselessweb.com/");
+    document.getElementById("lenda").innerHTML = 'O site que você acabou de abrir serviu de referência para esse aqui.';
+  }
+  if (contador == 12) {
+    document.getElementById("textoTooltip").innerHTML = 'Agora sim é o fim...';
+    document.getElementById("lenda").innerHTML = 'Fico lisonjeado de você ter voltado, mas infelizmente este foi o fim do coteúdo do site!';
+  }
+  if (contador == 13) {
+    document.getElementById("lenda").innerHTML = 'Muito obrigado pela participação!';
+  }
+  if (contador == 20) {
+    abrirLink("https://knowyourmeme.com/memes/2019-20-coronavirus-outbreak/photos/trending");
   }
 };
 
@@ -50,15 +84,12 @@ function botaoSoltar(){
 
 
 
-// Função do evento "mouseover"	para aparecer a textoTooltip
+// Função dos eventos "mouseover"	"mouseout" para a Tooltip do botão
 function tooltipAparecer(){
   document.getElementById("textoTooltip").style.visibility = "visible"
   document.getElementById("textoTooltip").style.opacity = "0.8"
 };
 
-
-
-// Função do evento "mouseout" para sumir a textoTooltip
 function tooltipDesaparecer(){
   document.getElementById("textoTooltip").style.visibility = "hidden"
   document.getElementById("textoTooltip").style.opacity = "0"
@@ -105,14 +136,14 @@ img.onclick = function(){
   modal.style.display = "block";
 }
 
-// Fechar o modal com (X)
+// Fechar o modal com 'X'
 var span = document.getElementsByClassName("close")[0];
 
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// Abrir modal assim que a página carrega
+// Abrir modal assim que a página é carregada
 window.onload = function(){
    setTimeout(function(){
       img.onclick();
